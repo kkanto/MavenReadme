507b4f77dcb747d4d90cdb13a44dd9699a61ef6f
kkanto <108447856+kkanto@users.noreply.github.com>
2022-12-22 16:11:43 +0100

    Create maven project

---
+++ src/main/java/de/allianz/mavenReadme/MavenReadmeApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.mavenReadme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReadmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenReadmeApplication.class, args);
+	}
+
+}
